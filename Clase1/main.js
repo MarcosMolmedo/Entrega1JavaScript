9ea1f0680cef8839127ea16cc3e7f842e1755b34
¨Marcos <marcosmolmedo@gmail.com>
2023-05-05 10:45:43 +0200

    se cambio el for por un swicht y el codigo se ejecuta correctamnete

--- Clase1/main.js
+++ Clase1/main.js
@@ -1,31 +1,36 @@
 let nombre = prompt("¡Bienvenido! ¿Cuál es tu nombre?");
-let respuesta = prompt("¿Vivís en Los Paises Bajos?, " + nombre + ", responde si o no");
+let respuesta = prompt("¿Vivís en Los Países Bajos?, " + nombre + ", responde si o no");
 
 if (respuesta.toLowerCase() === "si") {
-    alert("¡Genial, estás cada vez más cerca de vivir una experiencia culinaria que llevará todos tus sentidos al extremo!");
-    let ciudad = prompt("¿En qué ciudad?");
-    let favorita = prompt("¿Crees en las casualidades? Porque " + ciudad + " es mi ciudad favorita. ¿Cuántos invitados vas a tener? RECUERDA que podemos cotizar para 10, 20, 30 o 40 personas");
-    
-    let precioBase = 50; // Precio base por persona
-    let descuento = 0.1; // Descuento del 10% para grupos de más de 20 personas
+  alert("¡Genial, estás cada vez más cerca de vivir una experiencia culinaria que llevará todos tus sentidos al extremo!");
+  let ciudad = prompt("¿En qué ciudad?");
+  let cantidad = prompt("Mi ciudad favorita, ¿Para cuántas personas deseas una cotización? (10,20,30 o 40 personas)");
 
-    for (let i = 10; i <= 40; i += 10) {
-        let precioTotal = i * precioBase;
-    alert ((precioTotal = i * precioBase));     
+  let precioBase = 50;
+  let precioTotal;
 
-        if (i >= 20) {
-            precioTotal *= (1 - descuento); // Aplica descuento del 10% si hay más de 20 personas
-        }
-     
-        console.log("Para " + i + " personas, el precio total es de $" + precioTotal);
-    }
-   
+  switch (cantidad) {
+    case "10":
+      precioTotal = precioBase * 10;
+      alert(`Para 10 personas, el precio total es de €${precioTotal} euros.`);
+      break;
+    case "20":
+      precioTotal = precioBase * 20 * 0.9;
+      alert(`Para 20 personas, el precio total con descuento es de €${precioTotal} euros.`);
+      break;
+    case "30":
+      precioTotal = precioBase * 30 * 0.9;
+      alert(`Para 30 personas, el precio total con descuento es de €${precioTotal} euros.`);
+      break;
+    case "40":
+      precioTotal = precioBase * 40 * 0.9;
+      alert(`Para 40 personas, el precio total con descuento es de €${precioTotal} euros.`);
+      break;
+    default:
+      alert("Lo siento, no puedo cotizar lo solicitado. Por favor contáctanos por email.");
+  }
 } else if (respuesta.toLowerCase() === "no") {
-    alert("¡Uups! Por el momento trabajamos solo dentro de los Paises Bajos, pero pronto estaremos en otros países de la UE");
+  alert("Uups !! Estamos en Países Bajos, pero puedes recomendarnos con tus amigos.");
 } else {
-    alert("Lo siento, no entiendo tu respuesta. Por favor, responde sí o no.");
+  alert("No entiendo tu respuesta, por favor responde si o no.");
 }
-marcos
-
-
-
