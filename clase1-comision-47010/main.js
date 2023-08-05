d02dc023a3633eab57aafb2b3d925d1ab8977687
¨Marcos <marcosmolmedo@gmail.com>
2023-08-05 16:41:36 +0200

    se intenta solucionar error en correlacion pregunta -respuesta

--- clase1-comision-47010/main.js
+++ clase1-comision-47010/main.js
@@ -3,19 +3,19 @@ alert("Bienvenido/a a My Translator");
 
 alert("¡Soy una lingüista y traductora. Responde algunas preguntas para conocer qué servicios te interesan.");
 
-const necesitaTraduccion = prompt("¿Necesitas servicios de traducción? (Sí/No)");
-const necesitaInterpretacion = prompt("¿Necesitas servicios de interpretación? (Sí/No)");
-const necesitaClases = prompt("¿Necesitas clases personalizadas de inglés o español? (Sí/No)");
+const necesitaTraduccion = prompt("¿Necesitas servicios de traducción? (Si/No)");
+const necesitaInterpretacion = prompt("¿Necesitas servicios de interpretación? (Si/No)");
+const necesitaClases = prompt("¿Necesitas clases personalizadas de inglés o español? (Si/No)");
 
-if (necesitaTraduccion.toLowerCase() === 'sí') {
+if (necesitaTraduccion.toLowerCase() === 'si') {
   alert("Ofrezco servicios de traducción profesional en varios idiomas.");
 }
 
-if (necesitaInterpretacion.toLowerCase() === 'sí') {
+if (necesitaInterpretacion.toLowerCase() === 'si') {
   alert("Proporciono servicios de interpretación en eventos y reuniones.");
 }
 
-if (necesitaClases.toLowerCase() === 'sí') {
+if (necesitaClases.toLowerCase() === 'si') {
   alert("Ofrezco clases personalizadas de inglés y español en línea.");
 }
 
