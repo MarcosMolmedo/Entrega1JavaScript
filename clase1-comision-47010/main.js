208486bc0ebb77a3c69bd33b83f410cb43300fd7
¨Marcos <marcosmolmedo@gmail.com>
2023-08-05 17:25:54 +0200

    se agrega ciclo for y function

--- clase1-comision-47010/main.js
+++ clase1-comision-47010/main.js
@@ -3,20 +3,22 @@ alert("Bienvenido/a a My Translator");
 
 alert("¡Soy una lingüista y traductora. Responde algunas preguntas para conocer qué servicios te interesan.");
 
-const necesitaTraduccion = prompt("¿Necesitas servicios de traducción? (Si/No)");
-const necesitaInterpretacion = prompt("¿Necesitas servicios de interpretación? (Si/No)");
-const necesitaClases = prompt("¿Necesitas clases personalizadas de inglés o español? (Si/No)");
-
-if (necesitaTraduccion.toLowerCase() === 'si') {
-  alert("Ofrezco servicios de traducción profesional en varios idiomas.");
+function preguntarServicio(servicio) {
+  return prompt(`¿Necesitas servicios de ${servicio}? (Si/No)`).toLowerCase() === 'si';
 }
 
-if (necesitaInterpretacion.toLowerCase() === 'si') {
-  alert("Proporciono servicios de interpretación en eventos y reuniones.");
+const servicios = ["traducción", "interpretación", "clases personalizadas de inglés o español"];
+const intereses = [];
+
+for (const servicio of servicios) {
+  if (preguntarServicio(servicio)) {
+    intereses.push(servicio);
+    alert(`Ofrezco servicios de ${servicio} profesional en varios idiomas.`);
+  }
 }
 
-if (necesitaClases.toLowerCase() === 'si') {
-  alert("Ofrezco clases personalizadas de inglés y español en línea.");
+if (intereses.length === 0) {
+  alert("No seleccionaste ningún servicio. ¡Hasta luego!");
+} else {
+  alert(`Gracias por responder el cuestionario, ${nombre}. Has mostrado interés en los siguientes servicios: ${intereses.join(", ")}.`);
 }
-
-alert("Gracias por responder el cuestionario.");
