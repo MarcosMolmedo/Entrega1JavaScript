8d7d9e83c0e9516cd22f19bc48eff35bcc9071eb
¨Marcos <marcosmolmedo@gmail.com>
2023-08-19 22:53:38 +0200

    se agrega function, array y se amplia la interaccion con el usario, explicando valor de las clases

--- clase1-comision-47010/main.js
+++ clase1-comision-47010/main.js
@@ -8,19 +8,66 @@ function preguntarServicio(servicio) {
 }
 
 let contador = 0;
-const servicios = ["traducción", "interpretación", "clases personalizadas de inglés o español"];
+const servicios = ["traducción", "interpretación", "clases de inglés"];
 const intereses = [];
 
 while (contador < servicios.length) {
   if (preguntarServicio(servicios[contador])) {
     intereses.push(servicios[contador]);
-    alert(`Ofrezco servicios de ${servicios[contador]} profesional en varios idiomas.`);
+    alert(`Ofrezco servicios de ${servicios[contador]} profesional en en inglés y español`);
   }
   contador++;
+}
+
+let costoClases = 0;
+let tipoClases = "";
+
+function elegirTipoClases() {
+  tipoClases = prompt("Elige el tipo de clases: Grupales, Individuales o Personalizadas").toLowerCase();
+
+  switch (tipoClases) {
+    case "grupales":
+      costoClases = 15;
+      break;
+    case "individuales":
+      costoClases = 25;
+      break;
+    case "personalizadas":
+      costoClases = 30;
+      break;
+    default:
+      alert("Opción no válida, se tomará el costo de clases grupales por defecto.");
+      tipoClases = "grupales";
+      costoClases = 15;
+      break;
+  }
+}
+
+if (intereses.includes("clases de inglés")) {
+  elegirTipoClases();
+} else {
+  alert("No seleccionaste clases de inglés. ¡Hasta luego!");
+}
+
+const horariosRespuesta = {
+  lunesAViernes: "9:00 AM - 6:00 PM",
+  sabados: "10:00 AM - 2:00 PM"
+};
+
+let horarioSeleccionado = prompt("Selecciona un horario de respuesta: Lunes a Viernes o Sábados").toLowerCase();
+
+if (horariosRespuesta[horarioSeleccionado]) {
+  alert(`Nuestro horario de respuesta es: ${horariosRespuesta[horarioSeleccionado]}`);
+} else {
+  alert("Horario no válido, se tomará el horario de respuesta por defecto (Lunes a Viernes).");
+  horarioSeleccionado = "lunesAViernes";
 }
 
 if (intereses.length === 0) {
   alert("No seleccionaste ningún servicio. ¡Hasta luego!");
 } else {
   alert(`Gracias por responder el cuestionario, ${nombre}. Has mostrado interés en los siguientes servicios: ${intereses.join(", ")}.`);
+  if (tipoClases) {
+    alert(`Resumen de tu selección:\nNombre: ${nombre}\nServicios de interés: ${intereses.join(", ")}\nTipo de clases: ${tipoClases}\nCosto de clases: ${costoClases} euros\nHorario de respuesta: ${horariosRespuesta[horarioSeleccionado]}`);
+  }
 }
